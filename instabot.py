7552358c8e50a805bc6b143e70e51c8122e5c93f
njakhmola <jakhmolanidhi22@gmail.com>
2017-09-08 20:25:19 +0530

    Delete instabot.py

--- instabot.py
+++
@@ -1,8 +0,0 @@
-print "hello"
-import requests;
-def getDetails():
-    url = "https://api.instagram.com/v1/users/search?q=niravnikhil&access_token=5793537413.13b9c2a.0e37b8ae91714cdfab5ae87559175055";
-    obj = requests.get(url);
-    print obj.json();
-getDetails();
-
